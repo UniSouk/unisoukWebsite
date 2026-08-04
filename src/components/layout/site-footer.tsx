4d339e968d09cd14de2151d2a191bc7aea8bf34e
niru8756 <nirupama.singh@unisouk.com>
2026-08-04 17:30:28 +0530

    fix: correct footer facebook icon

--- src/components/layout/site-footer.tsx
+++ src/components/layout/site-footer.tsx
@@ -45,8 +45,7 @@ function SocialIcon({
 
   return (
     <svg viewBox="0 0 24 24" aria-hidden="true">
-      <circle cx="12" cy="12" r="9" />
-      <path d="M14.5 8h-1.7c-1.3 0-1.8.8-1.8 1.9V12h3l-.5 3H11v6" />
+      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3Z" />
     </svg>
   );
 }
